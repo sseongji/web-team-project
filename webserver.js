bfd27e1b40660fee909c6dca2a4f4ba789ece6d1
jnn08 <kkjnn08@gmail.com>
2022-12-20 00:22:40 +0900

    스터디 게시판 작업 중

--- webserver.js
+++ webserver.js
@@ -105,6 +105,11 @@ const appDir = path.dirname(require.main.filename);
 
 //routes
 
+//스터디 페이지
+app.get("/study", (req, res) => {
+  return res.render("study.ejs");
+});
+
 //비밀번호 찾기
 app.get("/findpassword", (req, res) => {
   return res.render("findpassword.ejs");
